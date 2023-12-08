cdc787dd8632f2e7f74b4a96885d7e235520a563
Ravi-Maloth <153194196+Ravi-Maloth@users.noreply.github.com>
2023-12-08 16:36:58 +0530

    Update views.py

--- mailapp/views.py
+++ mailapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
 
-
+print(10+20)
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
