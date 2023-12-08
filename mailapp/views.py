35f228eb28bdaf3a831b7ea4feba26fa44297002
Ravi-Maloth <malothravi866@gmail.com>
2023-12-08 16:35:06 +0530

    Initial commit

---
+++ mailapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def send(request):
+    x = request.GET["fn"]
+    y = request.GET["ln"]
+    z = request.GET["em"]
+    from_mail = settings.EMAIL_HOST_USER
+    to_list = [z]
+    subject='Hi'
+    msg = "Hi" + x + " " + y + "Thank You For Contacting Us"
+    send_mail(subject,msg, from_mail, to_list, fail_silently=False)
+    return HttpResponse("check your mail once")
